41cc8fb0ac6af59846663c3d6a3307ede93b7795
Chizindu <chizindu27@gmail.com>
2026-04-05 06:28:56 +0000

    Added Login Logic

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ class Recipe(db.Model):
     name = db.Column(db.String(100), nullable=False)
     servings = db.Column(db.String(10), default='4')
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
 #Create the database tables
 with app.app_context():
     db.create_all()
@@ -19,6 +24,42 @@ with app.app_context():
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            return redirect('/')
+        else:
+            return "Invalid login"
+
+    
+    return render_template('login.html')
+    
+@app.route('/create_account', methods = ['GET','POST'])
+def create_account():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+
+        #checks for existing user
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return "Username already exists"
+
+        #Confirms Passwords match
+        if password != confirm_password:
+            return "Passwords do not match"
+        #Save User
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/login')
+    return render_template('create_account.html')
 
 @app.route('/api/recipes', methods=['GET'])
 def get_recipes():
